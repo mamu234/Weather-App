3a35857f9d73f4858b807cc2222db2983216692b
carolyne <pcmaunda@gmail.com>
2022-09-20 09:54:46 +0300

    ft:redme features

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-## Description:This is a weather app that enable users search for the weather conditions in various cities all over the world 
+# Description
+The weather app enable users search for the weather conditions in various cities all over the world 
+
+Features
+
+## BDD (Behavior Driven Development)
+users can use the search feature to input the city and get the latest weather updates for the city
+Users can access weather for any city using the openweathermap API 
+Javascript promises and the fetch method for getting data  
+
+## Languages and Frameworks
+Html5 
+css 
+Javascript
+
+
+## Setup instructions
+Create a project folder
+Add the html ,css ,js and readme files 
+link the css, and js files to the html 
+<link rel="stylesheet" href="weather.css">
+<script src="weather.js"></script>
+
+Live Site: 
+
+Licence MIT LICENCE
+
--- weather.js
+++ weather.js
@@ -1,15 +1,15 @@
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
+var button = document.querySelector('.button')//grab the value for the button from the DOM
+var inputValue = document.querySelector('.inputValue')//grab the input value
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
 button.addEventListener('click', function(){
 fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=60b9236f1143b343a7c88b85319da931')
-.then(response => response.json())
-.then(data => {
+.then(response => response.json())// Using two prmoses then where we get a response using json format //
+.then(data => {//The second promise is for geting data form the API//
 
-var nameValue = data['name'];
+var nameValue = data['name']; //data comes from the API
 var descValue = data['weather'][0]['description'];
 var tempValue = data['main']['temp'];
 
@@ -22,3 +22,4 @@ temp.innerHTML = tempValue;
 })
 .catch(err => alert('wrong city name'))
 })
+// we use the function catch to look for any errors and alert the user 
